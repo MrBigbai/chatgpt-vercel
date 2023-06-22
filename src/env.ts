d9b9a3087b9f022a061840f00aad34a6d97431c6
131387999131 <2751014104@qq.com>
2023-06-22 13:43:49 +0800

    sync

--- src/env.ts
+++ src/env.ts
@@ -18,7 +18,8 @@ export const defaultEnv = {
     continuousDialogue: true,
     model: "gpt-3.5" as SimpleModel
   },
-  CLIENT_DEFAULT_MESSAGE: `- 您好，我知无不言，同时也是一个潘多拉宝盒。
+  CLIENT_DEFAULT_MESSAGE: `
+  - 您好，我知无不言，同时也是一个潘多拉宝盒。
   - 请联系站长Bai Sir获取密码
   - 现在支持多个对话，打开对话设置，点击新建对话。在输入框里输入 [[/]][[/]] 或者 [[空格]][[空格]] 可以切换对话，搜索历史消息。
   - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。
--- src/system.ts
+++ src/system.ts
@@ -10,8 +10,10 @@ export const defaultSetting = {
   model: "gpt-3.5-turbo" as Model
 }
 
-export const defaultMessage = `- 您好，我知无不言，同时也是一个潘多拉宝盒。
-- 联系站长Bai Sir获取密码
+export const defaultMessage = `
+- 您好，我知无不言，同时也是一个潘多拉宝盒。
+- 请联系站长Bai Sir获取密码
+- 现在支持多个对话，打开对话设置，点击新建对话。在输入框里输入 [[/]][[/]] 或者 [[空格]][[空格]] 可以切换对话，搜索历史消息。
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
 export type Setting = typeof defaultSetting
